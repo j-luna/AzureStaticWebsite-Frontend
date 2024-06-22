57060422082488837f0b01b57ca1dc84767b9287
Joe Lunaburg <38874911+j-luna@users.noreply.github.com>
2024-06-22 17:33:13 -0500

    Add frontend files to prep for upload to Storage Account

---
+++ html/javascript.js
@@ -0,0 +1,21 @@
+// Function to update the visitor count
+function updateVisitorCount() {
+    // Replace with your Azure Function URL
+    var functionUrl = 'https://fn-uzzvqttp5gorg.azurewebsites.net/api/visitorCount';
+
+    // Make a fetch request to the Azure Function
+    fetch(functionUrl)
+        .then(response => {
+            console.log('Response:', response);
+            return response.json();})
+        .then(data => {
+            console.log('Data:', data);
+            document.getElementById('CounterVisitor').textContent = data;
+        })
+        .catch(error => {
+            console.error('Error fetching visitor count:', error);
+        });
+}
+
+// Call the function on page load
+updateVisitorCount();
